2580ec5f4278607bb32ef6ec2f262964eb7184f2
Jay Hernandez <ajuan_jose@hotmail.com>
2020-10-24 15:08:54 -0600

    Font awesome added, node was updated so back to link tag for the stylesheets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
+
 //= require popper
 //= require bootstrap-sprockets
 
